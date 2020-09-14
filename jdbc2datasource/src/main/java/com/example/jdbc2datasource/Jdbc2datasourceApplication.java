274f12ad6ba01e44bf97fe6a5b0f7db9af6c9232
Sundarreva <42121081+Sundarreva@users.noreply.github.com>
2020-09-14 20:09:25 +0530

    Update Jdbc2datasourceApplication.java
    check git extension

--- jdbc2datasource/src/main/java/com/example/jdbc2datasource/Jdbc2datasourceApplication.java
+++ jdbc2datasource/src/main/java/com/example/jdbc2datasource/Jdbc2datasourceApplication.java
@@ -8,6 +8,7 @@ public class Jdbc2datasourceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jdbc2datasourceApplication.class, args);
+		//Changes in github
 	}
 
 }
